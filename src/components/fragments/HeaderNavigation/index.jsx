dc3e66ec5054f16ab01d844405cefdfa9a749f75
Adryanssyah <adryansyah434@gmail.com>
2023-12-20 17:47:43 +0700

    Fix - fixing z index overlapping by adding class z-50

--- src/components/fragments/HeaderNavigation/index.jsx
+++ src/components/fragments/HeaderNavigation/index.jsx
@@ -13,7 +13,7 @@ const HeaderNavigation = ({ children }) => {
      }, []);
 
      return (
-          <header className={`fixed -top-0 left-0 w-full flex justify-center border-b border-gray-200 dark:border-gray-900 ${scrolling ? 'bg-white/80 dark:bg-black/80 backdrop-blur-xl' : ''} `}>
+          <header className={`fixed -top-0 left-0 w-full flex justify-center border-b border-gray-200 dark:border-gray-900 z-50 ${scrolling ? 'bg-white/80 dark:bg-black/80 backdrop-blur-xl' : ''} `}>
                <nav className="w-full py-4 px-2 grid grid-cols-2 md:grid-cols-3 max-w-7xl">{children}</nav>
           </header>
      );
