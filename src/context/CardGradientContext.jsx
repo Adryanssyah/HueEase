85d37874ebb332bef2f954185a91aca581cb7db8
Adryanssyah <adryansyah434@gmail.com>
2023-12-20 17:48:56 +0700

    Add - adding new card gradient direction context

---
+++ src/context/CardGradientContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useEffect, useState } from 'react';
+
+const CardGradientDirectionContext = createContext();
+
+const CardGradientDirectionProvider = ({ children }) => {
+     const [cardGradientDirection, setCardGradientDirection] = useState('');
+
+     return <CardGradientDirectionContext.Provider value={{ cardGradientDirection, setCardGradientDirection }}>{children}</CardGradientDirectionContext.Provider>;
+};
+
+export const CardGradientDirection = CardGradientDirectionContext;
+export default CardGradientDirectionProvider;
