bb55e979f0119794d67f9602d08ab1774304318b
Adryanssyah <adryansyah434@gmail.com>
2023-12-20 17:43:33 +0700

    Update - add onclick event

--- src/components/elements/ButtonIcon/index.jsx
+++ src/components/elements/ButtonIcon/index.jsx
@@ -1,6 +1,6 @@
-const ButtonIcon = ({ children, title, color = '' }) => {
+const ButtonIcon = ({ children, title, color = '', onClick }) => {
      return (
-          <button className={`${color ? color : 'bg-darks dark:bg-white dark:text-darks hover:bg-[#2c2c2c] dark:hover:bg-slate-200 text-white'} px-5 py-2 rounded-lg font-medium text-sm mt-6 flex items-center gap-2`}>
+          <button onClick={onClick} className={`${color ? color : 'bg-darks dark:bg-white dark:text-darks hover:bg-[#2c2c2c] dark:hover:bg-slate-200 text-white'} px-5 py-2 rounded-lg font-medium text-sm mt-6 flex items-center gap-2`}>
                <span>{title}</span>
                {children}
           </button>
