19bc092221c84bf5f08002c06ae7b96edbd4e20a
Adryanssyah <adryansyah434@gmail.com>
2023-12-21 16:12:55 +0700

    Update - add link to my portfolio website

--- src/components/layouts/FooterLayout.jsx
+++ src/components/layouts/FooterLayout.jsx
@@ -3,7 +3,7 @@ const FooterLayout = () => {
           <footer className="w-full flex items-center justify-center">
                <div className="w-full py-10 max-w-7xl px-4 text-xs md:text-sm text-center md:text-start dark:text-gray-400 text-gray-500">
                     Built by{' '}
-                    <a href="#" className="underline">
+                    <a href="https://adryansportfolio.vercel.app/" target="_blank" className="underline">
                          Adryan
                     </a>{' '}
                     @2023. See the source code on{' '}
