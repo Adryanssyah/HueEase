f4ae271cc298fb2f70666df7f84cb6f14eb94a80
Adryanssyah <adryansyah434@gmail.com>
2023-12-27 23:01:35 +0700

    Fix - grammar

--- src/components/layouts/FooterLayout.jsx
+++ src/components/layouts/FooterLayout.jsx
@@ -2,11 +2,11 @@ const FooterLayout = () => {
      return (
           <footer className="w-full flex items-center justify-center">
                <div className="w-full py-10 max-w-7xl px-4 text-xs md:text-sm text-center md:text-start dark:text-gray-400 text-gray-500">
-                    Built by{' '}
+                    Crafted by{' '}
                     <a href="https://adryansportfolio.vercel.app/" target="_blank" className="underline">
                          Adryan
                     </a>{' '}
-                    @2023. See the source code on{' '}
+                    @2024. Explore the source code on{' '}
                     <a href="https://github.com/Adryanssyah/HueEase" target="_blank" className="underline">
                          Github
                     </a>
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,8 +6,8 @@ const CollectionPage = () => {
      return (
           <PagesLayout>
                <div className="px-2 mb-20">
-                    <h1 className="text-5xl font-semibold text-darks dark:text-white mb-8 xl:text-6xl">Collections</h1>
-                    <p className="text-gray-500 dark:text-gray-400 max-w-xl">This is all of your gradients color collections.</p>
+                    <h1 className="text-5xl font-semibold text-darks dark:text-white mb-8 xl:text-6xl">My Gradients</h1>
+                    <p className="text-gray-500 dark:text-gray-400 max-w-xl">This showcases all of your saved gradient color collections.</p>
                </div>
                <CollectionsLayout />
           </PagesLayout>
